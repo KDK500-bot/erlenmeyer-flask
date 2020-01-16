041bf9c1ab3ee674b7f941dc8b671fcd91dadc3a
Jonas <kdk500@web.de>
2020-01-16 11:04:02 +0100

    Eingabefehler Korrektur

--- aminoacid.c
+++ aminoacid.c
@@ -1,31 +1,37 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 int main(void)
 {
-    char amino[100];
-    printf("Aminoacidsequence: ");
-    scanf("%[^\n]", amino);
-    
-    int length = strlen(amino);
-    int basic = 0;
-    int space = 0;
-    
-    for (int i = 0; i < length; i++)
-    {
-        if((int)amino[i] == 72 || (int)amino[i] == 75 || (int)amino[i] == 82)
+        char amino[100];
+        printf("Aminoacidsequence: ");
+        scanf("%[^\n]", amino);
+
+        int length = strlen(amino);
+        int basic = 0;
+        int space = 0;
+
+        for (int i = 0; i < length; i++)
         {
-            basic++;           
+            if((int)amino[i] == 72 || (int)amino[i] == 75 || (int)amino[i] == 82)
+            {
+                basic++;
+            }
+            else if((int)amino[i] == 32)
+            {
+                space++;
+            }
+            else if(!isalpha(amino[i]) || !isupper(amino[i]))
+            {
+                printf("Please check your input! \n");
+                return 0;
+            }
         }
-        else if((int)amino[i] == 32)
-        {
-            space++;
-        }
-    }
-    int true_length = length - space;   
-    int calculus = basic * 100 / (true_length);
-    
-    printf("The aminoacidsequence contains %i aminoacids.\n", true_length);
-    printf("Basic aminoacids: %i\n", basic);
-    printf("%i%% of the sequence is basic.\n", calculus);
+        int true_length = length - space;   
+        int calculus = basic * 100 / (true_length);
+
+        printf("The aminoacidsequence contains %i aminoacids.\n", true_length);
+        printf("Basic aminoacids: %i\n", basic);
+        printf("%i%% of the sequence is basic.\n", calculus);
 }
