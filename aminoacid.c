0570cac6b9b865139b2cf5ed9918ec6b06420932
Jonas <kdk500@web.de>
2020-01-16 12:39:44 +0100

    changes

--- aminoacid.c
+++ aminoacid.c
@@ -1,16 +1,38 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-
+#include <unistd.h>
 int main(void)
 {
-        char amino[100];
+        char amino[20];
         printf("Aminoacidsequence: ");
-        scanf("%[^\n]", amino);
+        
+        //Einlesen
+        for (int i = 0 ; i < 20; i++){
 
+            char tmp; 
+            scanf("%[^\n]",&tmp);
+            amino[i] = tmp;
+
+        }
         int length = strlen(amino);
+        printf("Laenge: %i \n",length);
+      
+
+        //Ausgabe
+            char ausgabe; 
+        for (int i = 0 ; i < length; i++){
+            ausgabe = ausgabe + amino[i];
+            
+        printf("%i : Ausgabe: %c \n",i,ausgabe);
+        }    
+
+        sleep(10);
+        
         int basic = 0;
         int space = 0;
+    
+        
 
         for (int i = 0; i < length; i++)
         {
@@ -34,4 +56,5 @@ int main(void)
         printf("The aminoacidsequence contains %i aminoacids.\n", true_length);
         printf("Basic aminoacids: %i\n", basic);
         printf("%i%% of the sequence is basic.\n", calculus);
+        sleep(60);
 }
